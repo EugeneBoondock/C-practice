a9ed151d78f338c2b195b48cb4f026ec5b8eeb5a
Eugene Boondock <philosncube@gmail.com>
2022-08-21 22:49:02 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*
+   printf("WHAT I'VE LEARNT SO FAR\n");
+    printf("\n");
+   printf("Using char function\n");
+   char name[] = "Eugene";
+   printf("My name is %s\n", name);
+    printf("\n");
+   printf("Using int function\n");
+   int age = 24;
+   printf("I am %i years old\n", age);
+    printf("\n");
+   printf("Using float function\n");
+   float gpa = 3.5;
+   printf("My gpa is %f\n", gpa);
+    printf("\n");
+   printf("Using the constant function\n");
+    const int km = 97;
+    printf("I have travelled %i km", km);
+
+    printf("Getting User Input\n");
+
+    int age;
+    printf("Enter your age: ");
+    scanf("%i", &age);
+    printf("I am %i years old\n", age);
+
+    float gpa;
+    printf("Enter your gpa: ");
+    scanf("%f", &gpa);
+    printf("My gta is %f\n", gpa);
+
+    double temp;
+    printf("Enter your temp: ");
+    scanf("%lf", &temp);
+    printf("My temperature is %f\n", temp);
+
+    char grade;
+    printf("Enter your grade: ");
+    scanf("%c", &grade);
+    printf("My grade is %c", grade);
+
+
+*/
+
+/*
+    char name[20];
+    printf("Enter your name: ");
+    fgets(name, 20, stdin);
+    printf("My name is %s\n", name);
+    return 0;
+
+
+    int num1;
+    int num2;
+    printf("Enter first number: ");
+    scanf("%i", &num1);
+    printf("Enter second numer: ");
+    scanf("%i", &num2);
+
+    printf("Answer: %i", num1 + num2);
+
+
+    double gpa;
+    printf("Enter your gpa: ");
+    scanf("%lf", &gpa);
+    printf("Your gpa is %f", gpa);
+
+
+    printf("First attempt at Mad Libs Game");
+
+    char colour[20];
+    char noun[20];
+    char celebrity[20];
+    printf("Enter colour: ");
+    fgets(colour, 20, stdin);
+    printf("Enter noun: ");
+    fgets(noun, 20, stdin);
+    printf("Enter celebrity name: ");
+    fgets(celebrity, 20, stdin);
+
+    printf("Roses are %s", colour);
+    printf("%s are blue\n", noun);
+    printf("I love %s", celebrity);
+
+    char colour[20];
+    char noun[20];
+    char celebrity[20];
+    printf("Enter colour: ");
+    scanf("%s", coluor);
+    printf("Enter noun: ");
+    scanf("%s", noun);
+    printf("Enter celebrity name: ");
+    scanf("%s", celebrity);
+
+    printf("Roses are %s\n", colour);
+    printf("%s are blue\n", noun);
+    printf("I love %s\n", celebrity);
+*/
+
+/*
+    printf("code below doesn't work as intended");
+
+    char colour[20];
+    char noun[20];
+    char celebrity[20];
+    printf("Enter colour: ");
+    scanf("%s", colour);
+    printf("Enter noun: ");
+    scanf("%s", noun);
+    printf("Enter celebrity name: ");
+    fgets(celebrity, 20, stdin);
+
+    printf("Roses are %s\n", colour);
+    printf("%s are blue\n", noun);
+    printf("I love %s\n", celebrity);
+
+
+*/
+/*
+
+    printf("solution\n");
+
+    char colour[20];
+    char noun[20];
+    char celebrityF[20];
+    char celebrityL[20];
+
+    printf("Enter colour: ");
+    scanf("%s", colour);
+    printf("Enter noun: ");
+    scanf("%s", noun);
+    printf("Enter celebrity name: ");
+    scanf("%s%s", celebrityF, celebrityL);
+
+    printf("Roses are %s\n", colour);
+    printf("%s are blue\n", noun);
+    printf("I love %s %s\n", celebrityF,  celebrityL);
+
+    printf("Arrays");
+
+    int lottonumbers[] = {2, 3, 5, 6, 7, 8};
+    printf("I have %d cars", lottonumbers[4]);
+
+
+
+    int lottonumbers[] = {2, 3, 5, 6, 7, 8};
+    lottonumbers[2] = 5.6;
+    printf("I have %d cars", lottonumbers[2]);
+
+*/
+    print_line(int n);
+
+}
